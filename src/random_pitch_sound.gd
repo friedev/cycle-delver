f52b7f21cccbd49133b803ff26f6ad55a9d4d374
Aaron Friesen <aaron@frie.dev>
2026-05-23 21:39:20 -0500

    Remove unidiomatic use of self keyword

--- src/random_pitch_sound.gd
+++ src/random_pitch_sound.gd
@@ -2,11 +2,11 @@ class_name RandomPitchSound extends AudioStreamPlayer
 
 @export var pitch_scale_range := 0.25
 
-@onready var base_pitch_scale := self.pitch_scale
+@onready var base_pitch_scale := pitch_scale
 
 func randomize_and_play(from_position := 0.0) -> void:
-	self.pitch_scale = (
-		self.base_pitch_scale
-		+ randf_range(-1.0, +1.0) * self.pitch_scale_range
+	pitch_scale = (
+		base_pitch_scale
+		+ randf_range(-1.0, +1.0) * pitch_scale_range
 	)
 	super.play(from_position)
